dbe84b712688616ad69e55a30c8d2870bc976ded
Mateus Silva <representante.com.sp@gmail.com>
2022-05-29 16:34:52 -0300

    two commit

--- controller/routes.js
+++ controller/routes.js
@@ -4,6 +4,9 @@ const path = require('path')
 const bodyParser = require('body-parser')
 const conn = require('../model/database/payment')
 const payment = require('../model/database/payment')
+const user = require('../model/database/user')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const port = 3000
 app.listen(port)
 
@@ -14,21 +17,25 @@ app.set('view engine', 'ejs');
 app.set('views', './view/')
 
 app.get('/', (req, res) => {
-    res.render('index')
-})
-app.post('/', (req, res) => {
-    payment.create({
-        nome: req.body.nome,
-        senha: req.body.senha
-    }).then(() => {
-        // res.send('cadastrado com sucesso')
-        //direcionamento caso sucesso
-        res.redirect('/')
-    }).catch((err) => {
-        //res.send('erro ao cadastrar' + err)
-        //direcionamento caso erro
-        res.redirect('/')
+        res.render('index')
     })
+    /*app.post('/', (req, res) => {
+        payment.create({
+            nome: req.body.nome,
+            senha: req.body.senha
+        }).then(() => {
+            // res.send('cadastrado com sucesso')
+            //direcionamento caso sucesso
+            res.redirect('/')
+        }).catch((err) => {
+            //res.send('erro ao cadastrar' + err)
+            //direcionamento caso erro
+            res.redirect('/')
+        })
+    })*/
+app.post('/cadastrar', async(req, res) => {
+    console.log(req.body)
+    const password = await bcrypt.hash('123456', 8)
 })
 
 function teste(teste) {
