2439e9df6cd04dfc5682b5b229883231a82acb4f
Mateus Silva <representante.com.sp@gmail.com>
2022-10-07 20:14:38 -0300

    modified:   model/database/conn.js 	modified:   view/resources/css/cadastrar.css 	modified:   view/resources/css/index.css 	modified:   view/resources/css/produto.css 	new file:   view/resources/img/beirute/beirute.webp 	new file:   view/resources/img/lanche/beirute.webp 	new file:   view/resources/img/lanche/cachorro01.jpg 	new file:   view/resources/img/lanche/cachorro02.jpg 	new file:   view/resources/img/lanche/cachorro03.webp 	new file:   view/resources/img/lanche/pizza01.jpg 	new file:   view/resources/img/lanche/pizza02.jpg 	new file:   view/resources/img/lanche/salada.jpg 	new file:   view/resources/img/lanche/salada02.webp 	modified:   view/resources/js/controller.js 	modified:   view/template/navbar.ejs

--- view/resources/js/controller.js
+++ view/resources/js/controller.js
@@ -1,3 +1,41 @@
 const setItem = res => {
     console.log(res)
 }
+
+
+$(()=>{
+   
+    //Change()
+    validation();
+});
+
+function Change(){
+    var input = $('#formUser input');
+    input.on('change',(e)=>{
+        for(var i =0;i<input.length;i++){
+            console.log(input.get(i).value)
+            if(input.get(i).value == ''.trim()){
+                input.get(i).style.border = 'solid 2px red'
+            }
+            if(input.get(i).value != ''.trim()){
+                input.get(i).style.border = 'solid 2px green'
+            }
+        }
+    })
+}
+
+function validation(){
+    var input = $('#formUser input');
+    $('.btn-cadastrar-user').click(()=>{
+        Change()
+        for(var i =0;i<input.length;i++){
+            console.log(input.get(i).value)
+            if(input.get(i).value == ''.trim()){
+                input.get(i).style.border = 'solid 2px red'
+            }
+            if(input.get(i).value != ''.trim()){
+                input.get(i).style.border = 'solid 2px green'
+            }
+        }
+    });
+}
